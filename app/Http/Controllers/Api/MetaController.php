c0999ad3f629e926e7b9d59b1fe029e7cdd4d45f
Alexey Samoilik <samoilikalekseyservice@yandex.ru>
2025-11-11 19:18:05 +0300

    feat: add MetaController for change user roles

---
+++ app/Http/Controllers/Api/MetaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Enums\RoleEnum;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+class MetaController extends Controller
+{
+    public function roles(): JsonResponse
+    {
+        return response()->json(['roles' => array_values(RoleEnum::cases())]);
+    }
+}
