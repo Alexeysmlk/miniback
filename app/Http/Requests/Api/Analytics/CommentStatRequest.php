ba507c98078a7f7b3bbd01359cf2d45b235dc2f1
Alexey Samoilik <samoilikalekseyservice@yandex.ru>
2025-11-12 13:41:47 +0300

    feat: add comment stat request with rules

---
+++ app/Http/Requests/Api/Analytics/CommentStatRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Api\Analytics;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CommentStatRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'period' => [
+                'required',
+                'string',
+                Rule::in(['day', 'week', 'month']),
+            ],
+            'group_by' => [
+                'required',
+                'string',
+                Rule::in(['hour', 'day']),
+            ],
+        ];
+    }
+}
