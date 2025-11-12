442651793abb6fbe12777c961d992d4e6af7bfda
Alexey Samoilik <samoilikalekseyservice@yandex.ru>
2025-11-12 16:27:21 +0300

    ref: delete cache for roles list

--- app/Http/Controllers/Api/MetaController.php
+++ app/Http/Controllers/Api/MetaController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
 
 class MetaController extends Controller
 {
     public function roles(): JsonResponse
     {
-        return response()->json(['roles' => array_values(RoleEnum::cases())]);
+        return response()->json([
+            'roles' => array_values(RoleEnum::cases())
+        ]);
     }
 }
