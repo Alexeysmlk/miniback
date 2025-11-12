0a74d2c2a7d7ca45993d95f425caaf5ed4570c4b
Alexey Samoilik <samoilikalekseyservice@yandex.ru>
2025-11-12 16:23:07 +0300

    ref: update route file and name middleware

--- routes/api.php
+++ routes/api.php
@@ -13,47 +13,59 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-    Route::middleware('guest.api')->group(function () {
-        Route::post('/register', [AuthController::class, 'register']);
-        Route::post('/login', [AuthController::class, 'login']);
-    });
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('for-guest')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::controller(EmailVerificationController::class)->group(function () {
-        Route::post('/email/verification-notification', 'send')->name('verification.send');
-        Route::get('/email/verify/{id}/{hash}', 'verify')->middleware('signed')->name('verification.verify');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/user', fn (Request $request) => new UserResource($request->user()))->name('user.show');
+
+    Route::controller(EmailVerificationController::class)
+        ->prefix('email/verify')
+        ->name('verification.')
+        ->group(function () {
+            Route::post('/notification', 'send')->name('send');
+            Route::get('/{id}/{hash}', 'verify')->middleware('signed')->name('verify');
     });
 
     Route::middleware('verified')->group(function () {
-        Route::controller(PostController::class)->group(function () {
-            Route::get('/posts', 'index')->name('posts.index')->can('viewAny', Post::class);
-            Route::post('/posts', 'store')->name('posts.store')->can('create', Post::class);
-            Route::get('/posts/{post}', 'show')->name('posts.show')->can('view', 'post');
-            Route::patch('/posts/{post}', 'update')->name('posts.update')->can('update', 'post');
-            Route::delete('/posts/{post}', 'destroy')->name('posts.destroy')->can('delete', 'post');
-        });
+        Route::controller(PostController::class)
+            ->prefix('posts')
+            ->name('posts.')
+            ->group(function () {
+                Route::get('/', 'index')->name('index')->can('viewAny', Post::class)->middleware('throttle:api');
+                Route::post('/', 'store')->name('store')->can('create', Post::class);
+                Route::get('/{post}', 'show')->name('show')->can('view', 'post')->middleware('throttle:api');
+                Route::patch('/{post}', 'update')->name('update')->can('update', 'post');
+                Route::delete('/{post}', 'destroy')->name('destroy')->can('delete', 'post');
+            });
 
-        Route::controller(CommentController::class)->group(function () {
-            Route::get('/posts/{post}/comments', 'index')->name('posts.comments.index')->can('viewAny', Comment::class);
-            Route::post('/posts/{post}/comments', 'store')->name('posts.comments.store')->can('create', Comment::class);
-            Route::patch('/comments/{comment}', 'update')->name('comments.update')->can('update', 'comment');
-            Route::delete('/comments/{comment}', 'destroy')->name('comments.destroy')->can('delete', 'comment');
-        });
+        Route::controller(CommentController::class)
+            ->group(function () {
+                Route::get('/posts/{post}/comments', 'index')->name('posts.comments.index')->can('viewAny', Comment::class)->middleware('throttle:api');
+                Route::post('/posts/{post}/comments', 'store')->name('posts.comments.store')->can('create', Comment::class);
+                Route::patch('/comments/{comment}', 'update')->name('comments.update')->can('update', 'comment');
+                Route::delete('/comments/{comment}', 'destroy')->name('comments.destroy')->can('delete', 'comment');
+            });
 
-        Route::prefix('meta')->group(function () {
-            Route::get('/roles', [MetaController::class, 'roles'])->name('roles.index');
-        });
+        Route::patch('/users/{user}/role', [UserController::class, 'updateRole'])
+            ->name('users.update-role')
+            ->can('updateRole', 'user');
 
-        Route::get('/user', fn (Request $request) => new UserResource($request->user()));
-        Route::patch('/users/{user}/role', [UserController::class, 'updateRole'])->name('users.update-role')->can('updateRole', 'user');
+        Route::get('/meta/roles', [MetaController::class, 'roles'])
+            ->name('meta.roles')
+            ->middleware('throttle:api');
 
-        Route::controller(AnalyticsController::class)->prefix('analytics')->name('analytics.')->group(function () {
-            Route::get('/posts', 'posts')->name('posts');
-            Route::get('/comments', 'comments')->name('comments');
-            Route::get('/users', 'users')->name('users');
+        Route::controller(AnalyticsController::class)
+            ->prefix('analytics')
+            ->name('analytics.')
+            ->group(function () {
+                Route::get('/posts', 'posts')->name('posts');
+                Route::get('/comments', 'comments')->name('comments');
+                Route::get('/users', 'users')->name('users');
         });
     });
 });
